6cb9c6af5578ecaf073d0edc9fe54ef52ab76013
Brian Schimert <brian.schimert@gmail.com>
2021-11-03 13:18:36 -0600

    finish task 1, NotificationToast

--- client/src/components/NotificationToast.js
+++ client/src/components/NotificationToast.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import logo from '../images/sslogo2.png'
+import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
+
 
 class NotificationToast extends React.Component {
 
     render() {
         return (
-            <div>
-            <h1 style={{color : this.props.color}}> {this.props.message}  </h1>
-            <button onClick= {() => this.props.closeToast}>
-                   <FontAwesomeIcon icon="faWindowClose" />
+            <div style={{color : this.props.textColor, backgroundColor : this.props.backgroundColor}}>
+            {this.props.message} 
+            <button onClick= {() => this.props.closeToast()}>
+                   <FontAwesomeIcon icon={faWindowClose}/>
                 </button>
            
             </div>
