8aebab51a3166285ed08d3718fd471cc01939c78
Brian Schimert <brian.schimert@gmail.com>
2021-11-03 02:02:54 -0600

    Add NotificationToast.js

---
+++ client/src/components/NotificationToast.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../images/sslogo2.png'
+
+class NotificationToast extends React.Component {
+
+    render() {
+        return (
+            <h1> NotificationToast in development. </h1>
+        );
+    }   
+}
+
+export default NotificationToast;
